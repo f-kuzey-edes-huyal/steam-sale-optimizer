900761c225fc16dd7a8ba3f24e3feaf326b46b8a
f-kuzey-edes-huyal <kuzeyuvercinka+1@gmail.com>
2025-06-10 21:29:09 +0300

    ✨ Initial successful DAG running 🚀

--- scrape_main.py
+++
@@ -1,8 +0,0 @@
-from temp_scripts.steamdb_scrape import get_recent_games
-
-df = get_recent_games(pages=5)
-print(f"[INFO] Total recent games collected: {len(df)} 🎮")
-print(df.head())
-
-df.to_csv("steam_recent_games.csv", index=False)
-print("[✅] Data saved to 'steam_recent_games.csv' 📁")
