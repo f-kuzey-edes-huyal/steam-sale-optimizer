caafef8ec541cb1c6b9ce04896dd80bcb5b55084
f-kuzey-edes-huyal <kuzeyuvercinka+1@gmail.com>
2025-06-25 19:55:49 +0300

    🧹 Cleaned up the repo a bit — it was as cluttered as my mind!

--- config/constants.py
+++ config/constants.py
@@ -1,5 +1,5 @@
 MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
-EXPERIMENT_NAME = "Grizz does experiment tracking"
+EXPERIMENT_NAME = "Experiment: Included Competitor Pricing and Review Score"
 EXPERIMENT_NAME2 = "Experiment: No Competitor Pricing or Review Score"
 DATA_PATH = "data/combined4.csv"
 SEED = 42
