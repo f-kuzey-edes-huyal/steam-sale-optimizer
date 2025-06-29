8df220a61e930321dfdda333da01da6fdebf439d
f-kuzey-edes-huyal <kuzeyuvercinka+1@gmail.com>
2025-06-29 05:33:00 +0300

    Attempt to resolve issue with experiments not appearing in MLflow UI

--- config/constants.py
+++ config/constants.py
@@ -1,5 +1,7 @@
 MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
-#MLFLOW_TRACKING_URI_dag = "sqlite:////mlflow_db/mlflow.db"
+
+
+
 MLFLOW_TRACKING_URI_dag =  "http://mlflow:5000"
 EXPERIMENT_NAME = "Experiment: Included Competitor Pricing and Review Score"
 EXPERIMENT_NAME2 = "Experiment: No Competitor Pricing or Review Score"
