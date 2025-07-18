1c7a9b9f1d814c05e79fb11f6724f352efd0579e
f-kuzey-edes-huyal <kuzeyuvercinka+1@gmail.com>
2025-07-18 23:40:19 +0300

    Update model tracking URI for local experiment tracking and registry in local environment

--- config/constants.py
+++ config/constants.py
@@ -1,5 +1,6 @@
 #MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
 MLFLOW_TRACKING_URI ="http://mlflow:5000"
+MLFLOW_TRACKING_URI_local ="http://localhost:5000"
 #MLFLOW_TRACKING_URI_air =  "file:/opt/airflow/mlruns"
 EXPERIMENT_NAME = "Experiment: Included Competitor Pricing and Review Score"
 EXPERIMENT_NAME2 = "Experiment: No Competitor Pricing or Review Score"
